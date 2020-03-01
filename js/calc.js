64709f6cf963e0005161151ed886bf905872d48b
iñigo Vadillo <ivadillo6@gmail>
2020-03-01 21:02:24 +0100

    correcciones menores

--- js/calc.js
+++ js/calc.js
@@ -1,6 +1,11 @@
 function chargen() {
-    var x = Math.floor((Math.random() * 10) + 1);
+    var x = Math.floor((Math.random() * 6) + 1);
     document.getElementById("resulcalc").innerHTML = x;
   }
 
-  
+  switch(resulcalc){
+    case 1:
+    resulcalc = 1;
+		  document.write("<p>kk</p>");
+		  break;
+  }
