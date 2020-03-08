b944cf97726d6daaaea2893e789a3040f25e170d
iñigo Vadillo <ivadillo6@gmail>
2020-03-08 22:52:04 +0100

    arreglado tablas para ocultar div

--- js/tabla.js
+++ js/tabla.js
@@ -2,9 +2,9 @@
 function generar(){
 	// Selecting the input element and get its value 
 	let j = document.getElementById("numtab").value;
-	console.log(j);
+	//console.log("AQUI AL PRINCIPIO",j);
 
-
+/*
 	document.write("<ul>");
 	
 		for(i = 1;i<=10;i++){
@@ -16,22 +16,28 @@ function generar(){
 		document.write("</ul>");
 		document.write("<a href=tablas.html class=button>Volver a aprender con Capi</a>");
 }
-/*
+
 DESPUES DEL INNER PONER UN IGUAL =============================
+*/
 
 
-
-document.getElementById('tablas').innerHTML(zz"<ul>");
+document.getElementById('tablas').innerHTML = ("<ul>");
 	
 		for(i = 1;i<=10;i++){
-				document.getElementById('tablas').innerHTML("<li>");
-				document.getElementById('tablas').innerHTML(j + "x " + i + "= " + j * i);
-				document.getElementById('tablas').innerHTML("</li>");
+				document.getElementById('tablas').innerHTML += ("<li>");
+				document.getElementById('tablas').innerHTML += (j + "x " + i + "= " + j * i);
+				document.getElementById('tablas').innerHTML += ("</li>");
 			};
 	
-		document.getElementById('tablas').innerHTML("</ul>");
+		document.getElementById('tablas').innerHTML += ("</ul>");
+		document.getElementById('numtab').hidden += true;
+		document.getElementById('botonMult').hidden = true;
+		//document.getElementById('botonMult').style.display = "none";
+		
+}
 
 
+/*
 
 document.article.innerHTML("<ul>");
 	
